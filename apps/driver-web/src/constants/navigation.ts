5ec3c706fe04c8cc6bdda1e061476994a8925d70
Muhammed Bashir <bashmufol@gmail.com>
2026-07-01 08:21:41 +0100

    Stop tracking and remove apps

--- apps/driver-web/src/constants/navigation.ts
+++
@@ -1,3 +0,0 @@
-export const OPERATOR_APP_URL = import.meta.env.DEV
-  ? 'http://localhost:5174/operator/'
-  : '/operator/'
--- apps/operator-dashboard/src/constants/navigation.ts
+++
@@ -1,3 +0,0 @@
-export const DRIVER_APP_URL = import.meta.env.DEV
-  ? 'http://localhost:5173/'
-  : '/'
